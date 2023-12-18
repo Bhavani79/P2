46392cc3d19fe86bfed5258cfb20ae755e290951
Bhavani Chowdary Yarlagadda <bhavanichowdary79@hotmail.com>
2023-12-18 15:51:23 +0530

    azure

--- P2/Form1.Designer.cs
+++ P2/Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -73,16 +79,15 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(339, 206);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(259, 22);
             this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(339, 247);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(259, 22);
             this.textBox2.TabIndex = 4;
@@ -90,15 +95,15 @@
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(339, 289);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(259, 22);
             this.textBox3.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(637, 206);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(545, 362);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 108);
             this.button1.TabIndex = 6;
@@ -109,7 +114,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(460, 31);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(139, 44);
             this.button2.TabIndex = 7;
@@ -126,11 +131,67 @@
             this.textBox4.TabIndex = 8;
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(819, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(745, 289);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(674, 206);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(698, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Marks-1";
+         //   this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(773, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Marks-3";
+         //   this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(841, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Marks-2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -140,9 +201,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+        //    this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,6 +221,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- P2/Form1.cs
+++ P2/Form1.cs
@@ -34,8 +34,8 @@ namespace P2
                     connection.Open();
 
                     textBox4.Text = "CONNECTION ESTABLISHED";
-                  //  Form existingForm = new Form();
-                  //  existingForm.Show();
+                    //  Form existingForm = new Form();
+                    //  existingForm.Show();
                 }
                 catch (SqlException ex)
                 {
@@ -54,39 +54,43 @@ namespace P2
 
         private void button1_Click(object sender, EventArgs e)
         {
-          
-            
-            string connectionString = "Data Source=BHAVANI\\SQLEXPRESS01;Initial Catalog=p2;Integrated Security=SSPI;";
-            SqlConnection connection = new SqlConnection(connectionString);
+            string connectionString = "Data Source=BHAVANI\\SQLEXPRESS01;Initial Catalog=p2;Integrated Security=True;";
 
-            connection.Open();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
-            string value1 = textBox1.Text;
-            string value2 = textBox2.Text;
-            string value3 = textBox3.Text;
+                string value1 = textBox1.Text;
+                string value2 = textBox2.Text;
+                string value3 = textBox3.Text;
+                string value4 = textBox5.Text; // Add TextBox 5
+                string value5 = textBox6.Text; // Add TextBox 6
+                string value6 = textBox7.Text; // Add TextBox 7
 
-            string query = "INSERT INTO p2 (Name, ID, Attendance) VALUES (@Value1, @Value2, @Value3)";
-           
-            using (SqlCommand command = new SqlCommand(query, connection))
+                string query = "INSERT INTO p2 (Name, ID, Attendance, Marks_1, Marks_2, Marks_3) " +
+                               "VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Value1", value1);
                     command.Parameters.AddWithValue("@Value2", value2);
                     command.Parameters.AddWithValue("@Value3", value3);
+                    command.Parameters.AddWithValue("@Value4", value4);
+                    command.Parameters.AddWithValue("@Value5", value5);
+                    command.Parameters.AddWithValue("@Value6", value6);
                     command.ExecuteNonQuery();
                 }
-            
 
-            // Optional: Display a success message or perform any additional actions
-            MessageBox.Show("Data saved to the database.");
+                // Optional: Display a success message or perform any additional actions
+                MessageBox.Show("Data saved to the database.");
 
-            textBox1.Text = string.Empty;
-            textBox2.Text = string.Empty;
-            textBox3.Text = string.Empty;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+                textBox3.Text = string.Empty;
+                textBox5.Text = string.Empty; // Clear TextBox 5
+                textBox6.Text = string.Empty; // Clear TextBox 6
+                textBox7.Text = string.Empty; // Clear TextBox 7
+            }
         }
     }
 }
